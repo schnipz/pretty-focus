635f2dc5adb31b29e7416f9ba376ad6cc2213e63
Mike Parsons <schnipz@gmail.com>
2015-11-16 22:14:13 +0000

    Remove tests (not really needed for this module)

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,9 @@
 var gulp = require('gulp');
-var Server = require('karma').Server;
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var header = require('gulp-header');
-
-var pkg = require('./package.json');
-var banner = ['/**!',
-  ' * <%= pkg.name %> - <%= pkg.description %>',
-  ' * @version v<%= pkg.version %>',
-  ' * Copyright (c) <%= new Date().getFullYear() %> <%= pkg.author %>',
-  ' * @license <%= pkg.license %>',
-  ' */',
-  ''].join('\n');
 
 gulp.task('scripts', function() {
   gulp.src('./src/*.js')
@@ -21,27 +11,13 @@ gulp.task('scripts', function() {
     .pipe(jscs.reporter())
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(uglify())
-    .pipe(header(banner, {pkg: pkg}))
+    .pipe(uglify({preserveComments: 'license'}))
     .pipe(rename({
       extname: '.min.js'
     }))
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('test', function(done) {
-  new Server({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done).start();
-});
-
-gulp.task('tdd', function(done) {
-  new Server({
-    configFile: __dirname + '/karma.conf.js'
-  }, done).start();
-});
-
-gulp.task('develop', ['scripts', 'test'], function() {
-  gulp.watch(['./src/*.js', './test/*.spec.js'], ['scripts', 'tdd']);
+gulp.task('develop', ['scripts'], function() {
+  gulp.watch(['./src/*.js'], ['scripts']);
 });
